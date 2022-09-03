5aec119c82b505df3785ed358bd7adc10f880867
danielt.fekete <danielt.fekete@gmail.com>
2022-09-03 20:54:54 +0200

    feat: filter products
    -add filter by category
    -add filter by supplier
    -fix design bugs

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -1,12 +1,13 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.service.ProductService;
-import com.codecool.shop.config.TemplateEngineUtil;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -16,7 +17,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.*;
+import java.util.Enumeration;
 
 @WebServlet(urlPatterns = {"/shop"})
 public class ProductController extends HttpServlet {
@@ -25,19 +26,31 @@ public class ProductController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        ProductService productService = new ProductService(productDataStore,productCategoryDataStore);
-
-        Enumeration<String> params = req.getParameterNames();
-        while (params.hasMoreElements()){
-            String param = params.nextElement();
-            System.out.println(param+req.getParameter(param));
-        }
-        productCategoryDataStore.getAll().forEach(System.out::println);
+        SupplierDao supplierDataStorage = SupplierDaoMem.getInstance();
+        ProductService productService = new ProductService(productDataStore, productCategoryDataStore);
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         context.setVariable("categories", productCategoryDataStore.getAll());
-        context.setVariable("products", productDataStore.getAll());
+        context.setVariable("suppliers", supplierDataStorage.getAll());
+
+        Enumeration<String> params = req.getParameterNames();
+        if (params.hasMoreElements()) {
+            String param = params.nextElement();
+            System.out.println("Param: " + param + ", Value: " + req.getParameter(param));
+            switch (param) {
+                case "categoryId":
+                    context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(Integer.parseInt(req.getParameter(param)))));
+                    break;
+                case "supplierId":
+                    context.setVariable("products", productDataStore.getBy(supplierDataStorage.find(Integer.parseInt(req.getParameter(param)))));
+                    break;
+            }
+        } else {
+            context.setVariable("products", productDataStore.getAll());
+        }
+
+
         engine.process("product/index.html", context, resp.getWriter());
     }
 
--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -3,10 +3,10 @@ const urlParams = new URLSearchParams(window.location.search);
 
 function loadBackground() {
     console.log(urlParams)
-    if(urlParams.toString()===""){
+    if (urlParams.has("categoryId")) {
+        document.body.style.backgroundImage = "url('/static/img/productCategory_" + urlParams.get("categoryId") + ".jpg')"
+    } else {
         document.body.style.backgroundImage = "url('/static/img/background_main.jpg')"
-    } else if(urlParams.has("categoryId")){
-        document.body.style.backgroundImage = "url('/static/img/productCategory_"+urlParams.get("categoryId")+".jpg')"
     }
 }
 
